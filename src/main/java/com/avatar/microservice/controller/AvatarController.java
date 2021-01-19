0a3dae2d246f2638d700f4183ee64f2631d1001a
DamienBouvet22 <damien.bouvet@le-campus-numerique.fr>
2021-01-19 10:34:57 +0100

    ADD -  Open Api 3

--- src/main/java/com/avatar/microservice/controller/AvatarController.java
+++ src/main/java/com/avatar/microservice/controller/AvatarController.java
@@ -2,6 +2,12 @@ package com.avatar.microservice.controller;
 
 import com.avatar.microservice.dao.AvatarDao;
 import com.avatar.microservice.model.Avatar;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +20,13 @@ public class AvatarController {
     private AvatarDao avatarDao;
 
     //Get Avatar
+    @Operation(summary = "Get a list of avatars")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the avatar",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Avatar.class)) }),
+            @ApiResponse(responseCode = "404", description = "List of avatars not found",
+                    content = @Content) })
     @GetMapping(value = "Avatar")
     public List<Avatar> avatarsList() {
         return avatarDao.findAll();
